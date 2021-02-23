63841626533cb9cb95b9647afa17b3ba4cc13733
renantescaro <renantescaro@yahoo.com.br>
2021-02-23 20:53:46 -0300

    adicionando comentarios e variaveis globais

--- inicial.py
+++ inicial.py
@@ -10,6 +10,12 @@ class Inicial(tk.Frame):
         self.grid()
         self.criar_componentes()
 
+        self.img_altura  = 0
+        self.img_largura = 0
+        self.linha       = 0
+        self.coluna      = 0
+        self.aux         = 0
+
 
     def criar_componentes(self):
         # botão selecionar imagem
@@ -20,14 +26,29 @@ class Inicial(tk.Frame):
 
 
     def selecionar_imagem(self):
+        # seleciona imagem do computador
         caminho_imagem =  filedialog.askopenfilename(initialdir = '/',title = 'Select file',filetypes = (('PNG','*.png'),('JPG','*.jpg'),('Todos Arquivos','*.*')))
         if len(caminho_imagem) > 0:
+            # abre imagem selecionada
             image = Image.open(caminho_imagem)
-            photo = ImageTk.PhotoImage(image)
+            self.img_largura, self.img_altura = image.size
+            # diminui tamanho da imagem na tela
+            image = image.resize((round(100 / self.img_altura * self.img_largura), round(100)))
+            # componente pra mostrar a imagem
+            photo = ImageTk.PhotoImage( image )
             self.lbl_imagem = tk.Label(master=self.master, text = 'Imagem Origem', image=photo)
             self.lbl_imagem.image = photo
             self.lbl_imagem.grid(row=2, column=1)
-        print(caminho_imagem)
+            # variaveis para futura manipulação da imagem
+            self.linha  = self.img_altura
+            self.coluna = self.img_largura
+            self.aux = self.linha *  self.coluna
+
+    
+    def processar_imagem(self):
+        pass
+
+
 
 
 root = tk.Tk()
